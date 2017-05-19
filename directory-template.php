6daae53bf0a7002dc72e129fcc3b175f6e6f7272
Mobile User <mobile@199.sub-97-3-253.myvzw.com>
2017-05-19 11:09:37 +0100

    Updated to catch webmention replies using Semantic Linkbacks

--- directory-template.php
+++ directory-template.php
@@ -2,6 +2,8 @@
 
 /*
  Template Name: Directory
+ *
+ * @package Minmod
  */
 
 get_header(); ?>
@@ -10,7 +12,7 @@ get_header(); ?>
 
 		<main id="main" class="site-main" role="main">
 
-			<p> /* Insert introduction text of your choice here */ </p>
+			<p> /* Insert any intro text here */ </p>
 
 
 <?php
@@ -19,43 +21,50 @@ get_header(); ?>
 	$args = array(
         	'status' => 'approve',
         	'order'   => 'DESC',
-		'type' 	=> 'webmention'
 	);
 
-	$i = 0;			//initialise counter
+	$count = 0;			//initialise counter
 	$check = 'no';		//set author check
 	$people = array();	//initalise author array to check against
 
-	//add addresses to exclude from list host portion of URL only, include empty string
-	$exclusions = array ( 'add.domain.here', '' );		
+	//add addresses to exclude from list (without http(s)://), include empty string
+	$exclusions = array ( 'add.domain.here','' );
 
 	$wp_query->comments = get_comments( $args );
     
 	foreach ( $wp_query->comments as $comment ) {
-		$comment_id = get_comment_ID();
+
+		$wmreply = get_comment_meta( $comment->comment_ID, 'semantic_linkbacks_type', true );
 		$author = get_comment_author( $comment->comment_ID );
 		$author_url = get_comment_author_url();
 		$parse = parse_url($author_url);
 		$host = $parse['scheme'].'://'.$parse['host'];	//get just the domain portion of the URL
 
-		//ensure author domain isn't excluded or name is not blank
-		if (!in_array($parse['host'], $exclusions ) && ( '' != $author )) {
+		//check if comment is either a webmention or webmention reply (via Semantic Linkbacks) but NOT from micro.blog
+		if ( (($comment->comment_type == 'webmention' ) || ( $wmreply == 'reply' )) && ( $parse['host'] != 'micro.blog' ) ) {
 
-			//check if author has already been listed
-			for ($x = 0; $x <= $i; $x++) {
-    				if ( $author == $people[$x] ) {
-					$check = 'yes';
-				}
-			} 
+			//ensure author domain isn't excluded or name is not blank
+			if (!in_array($parse['host'], $exclusions ) && ( '' != $author )) {
+
+				//check if author has already been listed
+				for ($x = 0; $x <= $count; $x++) {
+    					if ( $author == $people[$x] ) {
+						$check = 'yes';
+					}
+				} 
 			
-			//if author not yet listed add them to the array and display their link
-			if ( $check != 'yes' ) {
-				$people[$i] = $author;
-				echo '<a href="' . $host . '">'. $author . '</a><br/>';
+				//if author not yet listed add them to the array and display their link
+				if ( $check != 'yes' ) {
+					$people[$count] = $author;
+					echo '<a class="directory-link" href="' . $host . '">'. $author . '</a><br/>';
+				}
+	
+				$count += 1;
+				$check = 'no';
 			}
-		$i += 1;
-		$check = 'no';
+
 		}
+
 	}
 
 ?>
